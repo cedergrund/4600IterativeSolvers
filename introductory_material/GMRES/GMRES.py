69938a65039f56697ea0502e8a781610db1f1523
Kevin Stull <kest3869@colorado.edu>
2023-11-16 14:26:29 -0700

    indexing fix

--- introductory_material/GMRES/GMRES.py
+++ introductory_material/GMRES/GMRES.py
@@ -1,9 +1,9 @@
 import numpy as np
 
-# not working
 # finding minimizer (y) not implemented
-# only works if n = dim(A)
-def gmres(A, b, x0, k=3, tol=1e-8):
+# only works if k = dim(A)[0]
+def gmres(A, b, x0, k, tol=1e-8):
+
     """
     GMRES algorithm for solving the linear system Ax = b.
 
@@ -20,14 +20,16 @@ def gmres(A, b, x0, k=3, tol=1e-8):
     - num_iter: Number of iterations performed.
     """
 
-    # initializations 
+    # shape compatibility 
     x0 = np.reshape(x0, [len(x0),])
     b = np.reshape(b, [len(b),])
-    H = np.zeros([k, k])
+
+    # initialization (HELP)
+    H = np.zeros(np.shape(A))
     V = np.zeros([len(b), k])
 
     # 1.1
-    r_init = b - np.dot(A,b)
+    r_init = b - A @ x0
 
     # 1.2
     v0 = r_init / np.linalg.norm(r_init)
@@ -35,28 +37,28 @@ def gmres(A, b, x0, k=3, tol=1e-8):
 
     # 2.1
     for j in range(k-1):
-        
+
         # 2.2
-        for i in range(j):
-            H[i,j] = np.dot(np.dot(A,V[:,j]), V[:,j])
+        for i in range(j+1):
+            H[i,j] = A @ V[:,j] @ V[:,j]
 
         # 2.3
-        vhat = np.dot(A,V[:,j])
-        for i in range(j):
-            vhat -= np.dot(H[i,j],V[:,i])
+        vhat = A @ V[:,j]
+        for i in range(j+1):
+            vhat -= H[i,j] * V[:,i]
 
         # 2.4
         H[j+1,j] = np.linalg.norm(vhat)
 
         # 2.5 
         V[:, j+1] = vhat / H[j+1, j]
-        
+
         # TODO 
         # Calculate minimizing vector y for ||Be_1 - H_ky||
         y = x0
-        
+
     # 3.1
-    x = x0 + np.dot(V,np.transpose(y))
+    x = x0 + V @ np.transpose(y)
 
     print('H')
     print(H)
@@ -68,8 +70,10 @@ def gmres(A, b, x0, k=3, tol=1e-8):
     return x, False, k 
 
 # test case 
-A = np.random.rand(3, 3)
-b = np.random.rand(3, 1)
-x0 =  np.random.rand(3, 1)
-solution, converged, num_iter = gmres(A, b, x0)
+size  = 3
+A = np.random.rand(size, size)
+b = np.random.rand(size, 1)
+x0 =  np.random.rand(size, 1)
+solution, converged, num_iter = gmres(A, b, x0, size)
+print('\n', "function returned")
 print(solution, converged, num_iter)
